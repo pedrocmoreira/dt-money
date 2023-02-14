073e997bfa6c4810e06304c94075f673ab52189d
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-02-14 20:20:59 -0300

    chore: instalando e configurando styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './styles/global';
+
+import { defaultTheme } from './styles/themes/default';
+
 export function App() {
 
   return (
-    <div className="App">
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle/>
       <h1>Hello World</h1>
-    </div>
+    </ThemeProvider>
   )
 }
 
