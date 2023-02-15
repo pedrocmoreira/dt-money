2152b5fb153ddd8041a99404da8774bcce55ff4c
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-02-15 00:25:54 -0300

    chore: tabela de transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,36 @@
 import { Header } from "../../components/Header";
+import { Summary } from "../Summary";
+import { PriceHiglight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
-  return(
+  return (
     <div>
-      <Header/>
+      <Header />
+      <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width='50%'>Desenvolvimento de site</td>
+              <td>
+                <PriceHiglight variant="income">R$ 12.000,00</PriceHiglight>
+              </td>
+              <td>Venda</td>
+              <td>15/02/2022</td>
+            </tr>
+            <tr>
+              <td width='50%'>Hamburguer</td>
+              <td>
+                <PriceHiglight variant="outcome"> - R$ 12.000,00</PriceHiglight>
+              </td>
+              <td>Venda</td>
+              <td>15/02/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+
     </div>
   )
 }
