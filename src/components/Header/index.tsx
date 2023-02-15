f7a60b39e08c25af573ba091a5db34db898189ee
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-02-15 01:45:06 -0300

    chore: adicionando modal para cadastro de transações e finalizando design

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,24 @@
+import * as Dialog from "@radix-ui/react-dialog";
+
+import logoImg from '../../assets/logo.svg';
+import { NewTransactionModal } from "../NewTransactionModal";
+
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 
-import logoImg from '../../assets/logo.svg'
-
 export function Header() {
-  return(
+  return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} alt="" />
 
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal/>
+        </Dialog.Root>
+
       </HeaderContent>
     </HeaderContainer>
   )
