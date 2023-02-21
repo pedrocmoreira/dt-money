73a93ec236ca10256194d72374cc9bafc872e25e
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-02-21 12:51:34 -0300

    chore: adicionando requisição para transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,35 +1,55 @@
+import { useEffect, useState } from "react";
+
 import { Header } from "../../components/Header";
 import { Summary } from "../Summary";
 import { SearchForm } from "./components/SearchForm";
 
 import { PriceHiglight, TransactionsContainer, TransactionsTable } from "./styles";
 
+interface TransactionDTO {
+  id: number;
+  description: string;
+  type: 'income' | 'outcome';
+  price: number;
+  category: string;
+  createdAt: string;
+}
+
 export function Transactions() {
+  const [transactions, setTransactions] = useState<TransactionDTO[]>([]);
+
+  async function loadTransactions() {
+    const response = await fetch('http://localhost:3333/transactions');
+    const data = await response.json();
+
+    setTransactions(data);
+  }
+
+  useEffect(() => {
+    loadTransactions();
+  }, []);
+
   return (
     <div>
       <Header />
       <Summary />
 
       <TransactionsContainer>
-        <SearchForm/>
+        <SearchForm />
         <TransactionsTable>
           <tbody>
-            <tr>
-              <td width='50%'>Desenvolvimento de site</td>
-              <td>
-                <PriceHiglight variant="income">R$ 12.000,00</PriceHiglight>
-              </td>
-              <td>Venda</td>
-              <td>15/02/2022</td>
-            </tr>
-            <tr>
-              <td width='50%'>Hamburguer</td>
-              <td>
-                <PriceHiglight variant="outcome"> - R$ 12.000,00</PriceHiglight>
-              </td>
-              <td>Venda</td>
-              <td>15/02/2022</td>
-            </tr>
+            {transactions.map(transaction => {
+              return (
+                <tr key={transaction.id}>
+                  <td width='50%'>{transaction.description}</td>
+                  <td>
+                    <PriceHiglight variant={transaction.type}>{transaction.price}</PriceHiglight>
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>{transaction.createdAt}</td>
+                </tr>
+              )
+            })}
           </tbody>
         </TransactionsTable>
       </TransactionsContainer>
