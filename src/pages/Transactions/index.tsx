83193e30359e9f5956babb1852cac03a32a0b97d
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-02-27 20:43:13 -0300

    chore: adicionando hook para summary

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 
 import { Header } from "../../components/Header";
 import { TransactionsContext } from "../../contexts/TransactionContextType";
